1155f39b36a5189f0be57d0324ae21885ec1842e
pti4life <kallai.roland@outlook.com>
2020-04-01 12:51:46 +0200

    default user added

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ start:
 ~: mvn install (in the project root folder)  
 ~: cd usermanager-microservice  
 ~: mvn thorntail:run
+
+default user:  
+email: mail@validmail.hu  
+pass: pass123
--- usermanager-core/src/main/resources/META-INF/data.sql
+++ usermanager-core/src/main/resources/META-INF/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO user(id, name, email, password) VALUES (1, 'name1', 'mail@validmail.hu', '$2y$12$t0OhS4j8pHfrt/6nIq9tdOyzqf3zL/SNOjF6Lf/ZxQ5kFU2SRQfBS');
+INSERT INTO user(id, name, email, password) VALUES (1, 'name1', 'mail@validmail.hu', '$2y$12$CxhncCA2/oN6lVpAYLTJWeXWNvr6c0xgp6uu/FUMFqi9zxRuesS1G');
 
 INSERT INTO address(id, postal_code, city, street, house_number, user_id) VALUES (1, 1111, 'city1', 'street1', 11, 1);
